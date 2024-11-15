87e80fa2c29e88ae3dc5c63bdc06b27d9d391a56
code with edison <edsnkvn@gmail.com>
2024-11-15 16:32:31 +0200

    Commit changes for src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import AddDoctor from './pages/AddDoctor';
+import MedicalRecords from './pages/MedicalRecords';
+import AccessControl from './pages/AccessControl';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50 flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/add-doctor" element={<AddDoctor />} />
+            <Route path="/records" element={<MedicalRecords />} />
+            <Route path="/access" element={<AccessControl />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
